7be9905e18c3783eb4aff235828db68d3c7ae181
Valentin Schneider <valentin.schneider@arm.com>
2018-04-15 18:03:36 +0100

    install.py: Factorize stuff

--- install.py
+++ install.py
@@ -5,11 +5,24 @@ import os
 PATH = os.path.dirname(os.path.realpath(__file__))
 HOME = os.path.expanduser("~")
 
-filespath = os.path.join(PATH, "files")
-files = os.listdir(filespath)
-for f in files:
-    linkpath = os.path.join(HOME, ".{}".format(f))
-    if os.path.exists(linkpath):
-        print("{} already exists, skipping".format(linkpath))
+def install(directory, path, dotify=False):
+    """
+    Create symlinks in 'path' to all files in 'directory'
+    """
+    if dotify:
+        prefix = "."
     else:
-        os.symlink(os.path.join(filespath, f), linkpath)
+        prefix = ""
+
+    for f in os.listdir(directory):
+        linkpath = os.path.join(path, "{}{}".format(prefix, f))
+
+        if os.path.exists(linkpath):
+            print("{} already exists, skipping...".format(linkpath))
+        else:
+            os.symlink(os.path.join(directory, f), linkpath)
+
+install(os.path.join(PATH, "files"), HOME, True)
+
+emacs_package_dir = os.path.join(HOME, ".emacs.d", "packages")
+install(os.path.join(PATH, "emacs-packages"), emacs_package_dir)
