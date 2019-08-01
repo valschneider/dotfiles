8cc756f89e81f7e6f6281eefdd3fd473d776c162
Valentin Schneider <valentin.schneider@arm.com>
2019-08-01 15:49:46 +0100

    emacs: custom-packages: Prevent flycheck checker duplicates

--- emacs-dir/init/custom-packages.el
+++ emacs-dir/init/custom-packages.el
@@ -16,10 +16,18 @@
   (interactive)
   (setq flycheck-kernel-use-clang (not flycheck-kernel-use-clang)))
 
+(defun flycheck-add-next-checker-safe (checker next-checker)
+  "Safely add a checker to the next-checker list (prevents duplicates).
+
+CHECKER is the flycheck checker to which NEXT-CHECKER will be chained."
+  (let ((next-checkers (flycheck-checker-get checker 'next-checkers)))
+    (if (not (member next-checker next-checkers))
+	(flycheck-add-next-checker checker next-checker))))
+
 (use-package flycheck-checkpatch
   :load-path "~/.emacs.d/packages/flycheck-checkpatch/"
   :config
   (eval-after-load 'flycheck
     '(progn
        (flycheck-checkpatch-setup)
-       (flycheck-add-next-checker 'kernel-make 'checkpatch-code))))
+       (flycheck-add-next-checker-safe 'kernel-make 'checkpatch-code))))
