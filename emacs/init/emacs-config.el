b4a0cd4a6f05a017db536a3865d311a2c4bf6448
Valentin Schneider <valentin.schneider@arm.com>
2020-01-06 12:04:57 +0000

    emacs: org: Add interactive function to take screenshots

--- emacs/init/emacs-config.el
+++ emacs/init/emacs-config.el
@@ -108,3 +108,25 @@
     (narrow-to-region (point) (mark))
     (goto-char (point-min))
     (while (search-forward "\n" nil t) (replace-match " " nil t))))
+
+;; https://delta.re/org-screenshot/
+(defun org-insert-screenshot ()
+  "Take a screenshot into a time stamped unique-named file in the
+same directory as the org-buffer and insert a link to this file."
+  (interactive)
+  (setq filename
+	(concat
+	 (make-temp-name
+	  (concat (file-name-nondirectory (buffer-file-name))
+		  "_imgs/"
+		  (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+  (unless (file-exists-p (file-name-directory filename))
+    (make-directory (file-name-directory filename)))
+  ; take screenshot
+  (if (eq system-type 'darwin)
+      (call-process "screencapture" nil nil nil "-i" filename))
+  (if (eq system-type 'gnu/linux)
+      (call-process "import" nil nil nil filename))
+  ; insert into file if correctly taken
+  (if (file-exists-p filename)
+    (insert (concat "[[file:" filename "]]"))))
